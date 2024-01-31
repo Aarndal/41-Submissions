702f2f8fbc95d5ca2bf5f1e2c2b3a73a42e54aa7
Aaron Dahlhauser <aarndal@duck.com>
2024-01-31 19:58:39 +0100

    Initialized Sorting-Algorithms. Finished BubbleSort class.

---
+++ 41-04 - Sortier-Algorithmen/Sorting-Algorithms/Sorting-Algorithms/BubbleSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms
+{
+    internal class BubbleSort
+    {
+        public static int[] SortAscending(int[] _array)
+        {
+            int[] sortedArray = _array;
+
+            for (int i = 0; i < _array.Length - 1; i++)
+            {
+                for (int j = 0; j < _array.Length - 1 - i; j++)
+                {
+                    if (_array[j] > _array[j + 1])
+                    {
+                        (_array[j + 1], _array[j]) = (_array[j], _array[j + 1]);
+                    }
+                }
+            }
+
+            return sortedArray;
+        }
+
+        public static int[] SortDescending(int[] _array)
+        {
+            int[] sortedArray = _array;
+
+            for (int i = 0; i < _array.Length - 1; i++)
+            {
+                for (int j = 0; j < _array.Length - 1 - i; j++)
+                {
+                    if (_array[j] < _array[j + 1])
+                    {
+                        (_array[j], _array[j + 1]) = (_array[j + 1], _array[j]);
+                    }
+                }
+            }
+
+            return sortedArray;
+        }
+
+        public static int[] SortZickZack(int[] _array)
+        {
+            int[] sortedArray = new int [_array.Length];
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                if (i % 2 != 0)
+                    sortedArray[i] = _array[i / 2];
+                else
+                    sortedArray[i] = _array[_array.Length - 1 - i / 2];
+            }
+
+            return sortedArray;
+        }
+    }
+}
---
+++ 41-04 - Sortier-Algorithmen/Sorting-Algorithms/Sorting-Algorithms/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Sorting_Algorithms
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Bitte gib an, wie viele zufällige Zahlen von 1 bis 100 generiert werden sollen: ");
+            string? input = Console.ReadLine();
+
+            int count = 0;
+            if (input is not null)
+                count = int.Parse(input);
+
+            int[] array = new int[count];
+            Random rnd = new();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 101);
+            }
+
+            Console.WriteLine("\nUnsortiert:");
+
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("\n\nAufsteigend sortiert:");
+
+            int[] ascendingArray = BubbleSort.SortAscending(array);
+
+            foreach (int number in ascendingArray)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("\n\nAbsteigend sortiert:");
+
+            int[] descendingArray = BubbleSort.SortDescending(array);
+
+            foreach (int number in descendingArray)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("\n\nZickZack sortiert:");
+
+            int[] zickzackArray = BubbleSort.SortZickZack(array);
+
+            foreach (int number in zickzackArray)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
